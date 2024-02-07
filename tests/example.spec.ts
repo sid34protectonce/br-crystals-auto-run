646d6c1fd3a9f6a2a1c992d19c502c01735a6338
sid34protectonce <siddharth.s@protectonce.com>
2024-02-07 12:52:44 +0530

    added  script for broken crystal traffic

---
+++ tests/example.spec.ts
@@ -0,0 +1,102 @@
+import { test, Page } from '@playwright/test';
+
+test('Traffic Generation for broken Crystals App', async ({ page }) => {
+  const url: string = ' http://52.14.7.134:3000/';
+  await generateTraffic(page, url);
+});
+
+function getRandomEmail(): string {
+  var chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
+  var string = '';
+  for (var ii = 0; ii < 15; ii++) {
+    string += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return string + '@gmail.com';
+}
+
+function getRandomName(): string {
+  var chars = 'abcdefghijklmnopqrstuvwxyz';
+  var string = '';
+  for (var ii = 0; ii < 10; ii++) {
+    string += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return string;
+}
+
+function getRandomNumber(): number {
+  return Math.floor(Math.random() * 1000000);
+}
+
+async function generateTraffic(page: Page, url: string) {
+  const email = getRandomEmail();
+  const first_name = getRandomName();
+  const last_name = getRandomName();
+  const card_number = getRandomNumber();
+  const ph_number = getRandomNumber();
+  const password = "test123";
+  await page.goto(url);
+  await page.waitForTimeout(2000);
+  await page.getByRole('link', { name: 'Sign in', exact: true }).click();
+  await page.getByRole('link', { name: 'Sign Up Here' }).click();
+  await page.getByPlaceholder('First name').fill(first_name);
+  await page.getByPlaceholder('Last name').fill(last_name);
+  await page.getByPlaceholder('Company').fill('po');
+  await page.getByPlaceholder('Card number').fill(`${card_number}`);
+  await page.getByPlaceholder('Phone number').fill(`${ph_number}`);
+  await page.getByPlaceholder('Email').fill(email);
+  await page.getByPlaceholder('Password').fill(password);
+  await page.waitForTimeout(1000);
+  await page.getByRole('button', { name: 'register' }).click();
+
+  // login
+  await page.waitForTimeout(10000);
+  await page.getByPlaceholder('Email').fill(email);
+  await page.getByPlaceholder('Password').fill(password);
+  await page.getByRole('button', { name: 'sign in' }).click();
+
+  // activity
+  await page.waitForLoadState();
+  await page.getByRole('link', { name: 'Marketplace' }).click();
+  await page.getByTitle('Amethyst').click();
+  await page.goto(`${url}/marketplace`);
+
+  // testimonial
+  await page.getByRole('link', { name: 'Marketplace' }).click();
+  await page.getByPlaceholder('Your Name').fill(first_name);
+  await page.getByPlaceholder('Your Title').fill('po');
+  await page.getByPlaceholder('Testimonial').fill('hello');
+  await page.getByRole('button', { name: 'Send Testimonial' }).click();
+  await page.reload();
+
+  // message on home screen
+  await page.getByRole('link', { name: 'Home' }).click();
+  await page.getByPlaceholder('Your Name').fill(first_name);
+  await page.getByPlaceholder('Your Email').fill(email);
+  await page.getByPlaceholder('Subject').fill('Hello');
+  await page.getByPlaceholder('Message').fill('Hello world');
+  await page.getByRole('button', { name: 'Send Message' }).click();
+  await page.waitForLoadState();
+
+  // edit user details
+  await page.getByRole('link', { name: 'Edit user data' }).click();
+  await page.waitForTimeout(3000);
+  await page.getByPlaceholder('First name').click();
+  // await page.getByPlaceholder('First name').fill(`${first_name}h`);
+  await page.getByRole('button', { name: 'Save changes' }).click();
+
+  // various login activity
+  await page.getByRole('link', { name: `Log out ${first_name} ${last_name}` }).click();
+  await page.waitForTimeout(1000);
+  await page.getByRole('link', { name: 'Sign in', exact: true }).click();
+
+  const options = ['html', 'csrf', 'csrf_dom', 'oidc']
+
+  for(const option of options) {
+    await page.getByRole('combobox').selectOption(option);
+    await page.getByPlaceholder('Email').fill(email);
+    await page.getByPlaceholder('Password').fill('test123');
+    await page.getByRole('button', { name: 'sign in' }).click();
+    await page.waitForTimeout(1000);
+    await page.getByRole('link', { name: `Log out ${first_name} ${last_name}` }).click();
+  }
+}
